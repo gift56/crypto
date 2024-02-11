7f565b04b394b7187be98f22fd277b2c9c855829
Efe G <efeasiughu@gmail.com>
2024-02-11 13:58:53 -0500

    working on profile status smart contract

--- src/ProfileStatus.sol
+++ src/ProfileStatus.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
+contract ProfileStatus {
+    struct Status {
+        string statusMessage;
+        bool exists;
+    }
+
+    constructor() {}
+}
