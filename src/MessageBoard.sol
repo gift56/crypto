482eb668c0a52d1a35aee9474e714e744f5845a4
Efe G <efeasiughu@gmail.com>
2024-02-11 13:38:58 -0500

    working on smart contract

--- src/MessageBoard.sol
+++ src/MessageBoard.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
+contract MessageBoard {
+    address public owner;
+    string[] public messages;
+
+    event NewMessage(address indexed owner, string messages);
+
+    constructor() {
+        owner = msg.sender;
+    }
+}
