c358835d139a3e0cf9f52e133e7cd892c5102c4f
Efe G <efeasiughu@gmail.com>
2024-02-12 17:06:28 -0500

    working on some updated on contract metadata

--- src/HelloWorld.sol
+++ src/HelloWorld.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "@thirdweb-dev/contracts/extension/ContractMetadata.sol";
+
 
 contract HelloWolrd {
     string public message;
